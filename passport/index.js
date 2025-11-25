56553267ab92acf3ff85ac5190be7eada7b44e45
1028ragon <1028ragon@naver.com>
2025-11-25 09:49:34 +0900

    Chore: 코드 주석 추가

--- passport/index.js
+++ passport/index.js
@@ -1,19 +1,20 @@
 const passport = require('passport');
-const local = require('./localStrategy');
-const kakao = require('./kakaoStrategy');
+const local = require('./localStrategy'); // 로컬 로그인 (이메일/비밀번호)
+const kakao = require('./kakaoStrategy'); // 카카오 로그인 전략
 const User = require('../models/user');
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user.id); // 로그인 성공 -> 세션에 user.id 저장
   });
-
+  // serializeUser에서 저장한 id로 DB에서 사용자 정보를 다시 조회함
+  // 세션에 저장된 id를 실제 사용자 정보로 복원하는 과정
   passport.deserializeUser((id, done) => {
     User.findOne({ where: { id } })
-      .then(user => done(null, user))
+      .then(user => done(null, user)) // 조회된 user 객체를 req.user에 넣어줌
       .catch(err => done(err));
   });
 
-  local();
-  kakao();
+  local(); // localStrategy.js 안의 LocalStrategy 설정 실행
+  kakao(); // kakaoStrategy.js(아직 안만들음) 안의 KakaoStrategy 설정 실행
 };
--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy; // 이메일/비밀번호로 로그인하게끔
+const bcrypt = require('bcrypt'); // 해싱 라이브러리
 
-const User = require('../models/user');
+const User = require('../models/user'); // DB의 user모델
 
 module.exports = () => {
   passport.use(new LocalStrategy({
@@ -10,19 +10,19 @@ module.exports = () => {
     passwordField: 'password',
     passReqToCallback: false,
   }, async (email, password, done) => {
-    try {
+    try {  // 1) 이메일로 가입된 유저가 있는지 DB에서 찾기
       const exUser = await User.findOne({ where: { email } });
-      if (exUser) {
+      if (exUser) {  // 2) 비밀번호가 일치하는지 bcrypt.compare로 체크
         const result = await bcrypt.compare(password, exUser.password);
         if (result) {
-          done(null, exUser);
+          done(null, exUser); // 비번 일치
         } else {
-          done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
+          done(null, false, { message: '비밀번호가 일치하지 않습니다.' }); // 비번 틀림
         }
-      } else {
+      } else { // 유저 자체가 존재하지 않음
         done(null, false, { message: '가입되지 않은 회원입니다.' });
       }
-    } catch (error) {
+    } catch (error) {  // DB 오류 등 예상치 못한 에러
       console.error(error);
       done(error);
     }
